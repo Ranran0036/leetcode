646182c90a87de9b6058dcb5bf2556e0aa18bf69
Ranran <ranran0036@163.com>
2020-06-12 17:55:40 +0800

    Roman to integer

---
+++ RomantoInteger.js
@@ -0,0 +1,36 @@
+let romanToInt = function(s) {
+    let intRomanMap = new Map([
+        ['I',1],
+        ['V',5],
+        ['X',10],
+        ['L',50],
+        ['C',100],
+        ['D',500],
+        ['M',1000]
+    ]);
+
+    let res = 0;
+    let lastAdd = 0;
+
+    for(let i = 0; i < s.length; i++){
+
+        if(lastAdd < intRomanMap.get(s[i])){
+            res = res - lastAdd * 2 + intRomanMap.get(s[i]);
+        }else{
+            res += intRomanMap.get(s[i]);
+        }
+
+        lastAdd = intRomanMap.get(s[i]);
+
+    }
+
+    return res;
+    
+};
+
+console.log(romanToInt('III'));
+console.log(romanToInt('IV'));
+console.log(romanToInt('IX'));
+console.log(romanToInt('LVIII'));
+console.log(romanToInt('MCMXCIV'));
+console.log(romanToInt('CCII'));
