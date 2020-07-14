10d87885839bdbeb6dbb06847684ed92d422a842
Ranran <ranran0036@163.com>
2020-07-14 20:46:03 +0800

    triangle.js

---
+++ Triangle.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+let minimumTotal = (triangle) => {
+    const dp = new Array(triangle[triangle.length - 1].length);
+    // base case 是最后一行
+    for (let i = 0; i < dp.length; i++) {
+        dp[i] = triangle[triangle.length - 1][i];
+    }
+    // 从倒数第二列开始迭代
+    for (let i = dp.length - 2; i >= 0; i--) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            dp[j] = Math.min(dp[j], dp[j + 1]) + triangle[i][j];
+        }
+    }
+    return dp[0];
+};
