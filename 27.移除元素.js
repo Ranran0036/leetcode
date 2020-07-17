c0fc63b6a95d4c6ba390fcfea56cc5633f6c5174
Ranran <ranran0036@163.com>
2020-07-17 18:02:04 +0800

    [27] 移除元素

---
+++ 27.移除元素.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=27 lang=javascript
+ *
+ * [27] 移除元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+let removeElement = (nums, val) => {
+    let i = 0;
+
+    for(let j = 0; j < nums.length ; j++){
+        if(nums[j] != val){
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+
+    return i;
+};
+// @lc code=end
+
