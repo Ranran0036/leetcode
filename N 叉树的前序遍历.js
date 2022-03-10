186060d8b868c2ed7dd4c308674f9a1a11e0febf
Ranran <ranran0036@163.com>
2022-03-10 14:25:17 +0800

    N 叉树的前序遍历.js

---
+++ N 叉树的前序遍历.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+let preorder = function(root) {
+    const res = [];
+    if (root == null) {
+        return res;
+    }
+
+    const stack = [];
+    stack.push(root);
+    while (stack.length) {
+        const node = stack.pop();
+        res.push(node.val);
+        for (let i = node.children.length - 1; i >= 0; --i) {
+            stack.push(node.children[i]);
+        }
+    }
+    return res;
+};
