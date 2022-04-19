6a222bdba692963c3ca1d78151c22d19883b2a03
Ranran <ranran0036@163.com>
2022-04-20 03:31:19 +0800

    388. 文件的最长绝对路径.js

---
+++ 388. 文件的最长绝对路径.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} input
+ * @return {number}
+ */
+const lengthLongestPath = function (input) {
+    const n = input.length;
+    let pos = 0;
+    let ans = 0;
+    const level = new Array(n + 1).fill(0);
+
+    while (pos < n) {
+        /* 检测当前文件的深度 */
+        let depth = 1;
+        while (pos < n && input[pos] === "\t") {
+            pos++;
+            depth++;
+        }
+        /* 统计当前文件名的长度 */
+        let len = 0;
+        let isFile = false;
+        while (pos < n && input[pos] !== "\n") {
+            if (input[pos] === ".") {
+                isFile = true;
+            }
+            len++;
+            pos++;
+        }
+        /* 跳过换行符 */
+        pos++;
+
+        if (depth > 1) {
+            len += level[depth - 1] + 1;
+        }
+        if (isFile) {
+            ans = Math.max(ans, len);
+        } else {
+            level[depth] = len;
+        }
+    }
+    return ans;
+};
+
+let str = "dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext";
+console.log(lengthLongestPath(str));
