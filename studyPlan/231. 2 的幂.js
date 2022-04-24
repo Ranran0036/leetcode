f01f9f3247fe772e2b487c2d0127356ecb6abd4e
Ranran <ranran0036@163.com>
2022-04-25 01:42:50 +0800

    anothor solution : studyPlan\231. 2 的幂.js

--- studyPlan/231. 2 的幂.js
+++ studyPlan/231. 2 的幂.js
@@ -3,5 +3,6 @@
  * @return {boolean}
  */
 const isPowerOfTwo = function (n) {
-    return n > 0 && (n & (n - 1)) === 0;
+    //return n > 0 && (n & (n - 1)) === 0;
+    return n > 0 && (n & -n) === n;
 };
