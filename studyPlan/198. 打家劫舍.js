b5e8a48bef1979e15d17062ec0ca8299cd31c537
Ranran <ranran0036@163.com>
2022-04-24 20:51:10 +0800

    improve : studyPlan\198. 打家劫舍.js

--- studyPlan/198. 打家劫舍.js
+++ studyPlan/198. 打家劫舍.js
@@ -3,15 +3,19 @@
  * @return {number}
  */
 const rob = function (nums) {
-    if (nums.length === 0) return 0;
-    if (nums.length === 1) return nums[0];
-    if (nums.length === 2) return Math.max(nums[0], nums[1]);
-    let first = nums[0];
-    let second = Math.max(nums[0], nums[1]);
-    for (let i = 2; i < nums.length; i++) {
-        let third = first + nums[i];
-        first = second;
-        second = Math.max(second, third);
+    if (nums == null || nums.length == 0) {
+        return 0;
+    }
+    let length = nums.length;
+    if (length == 1) {
+        return nums[0];
+    }
+    let first = nums[0],
+        second = Math.max(nums[0], nums[1]);
+    for (let i = 2; i < length; i++) {
+        let temp = second;
+        second = Math.max(first + nums[i], second);
+        first = temp;
     }
     return second;
 };
