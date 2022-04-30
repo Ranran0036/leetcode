389da8aa3420459b72b7fc481f7d716acc38fd42
Ranran <ranran0036@163.com>
2022-04-30 11:45:22 +0800

    improve : studyPlan\Dynamic Programming---Day_4

--- studyPlan/Dynamic Programming/918. 环形子数组的最大和.js
+++ studyPlan/Dynamic Programming/918. 环形子数组的最大和.js
@@ -9,7 +9,7 @@ const maxSubarraySumCircular = function (nums) {
         minSum = nums[0],
         sum = 0;
 
-    nums.forEach((num) => {
+    for (const num of nums) {
         sum += num;
         //最大子数组和
         preMax = Math.max(preMax + num, num);
@@ -17,7 +17,7 @@ const maxSubarraySumCircular = function (nums) {
         //最小子数组和
         preMin = Math.min(preMin + num, num);
         minSum = Math.min(minSum, preMin);
-    });
+    }
 
     return maxSum > 0 ? Math.max(maxSum, sum - minSum) : maxSum;
 };
