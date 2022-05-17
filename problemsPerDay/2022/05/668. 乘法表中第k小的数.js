e750f7869d5bbb34dd718f6b020de23d71e24832
Ranran <ranran0036@163.com>
2022-05-18 00:33:57 +0800

    problemsPerDay\2022\05\668. 乘法表中第k小的数.js

--- problemsPerDay/2022/05/668. 乘法表中第k小的数.js
+++ problemsPerDay/2022/05/668. 乘法表中第k小的数.js
@@ -4,4 +4,41 @@
  * @param {number} k
  * @return {number}
  */
-const findKthNumber = function (m, n, k) {};
+const findKthNumber = function (m, n, k) {
+    let left = 1,
+        right = m * n + 1;
+
+    while (left + 1 !== right) {
+        const mid = (left + right) >> 1;
+
+        if (check(m, n, k, mid)) {
+            right = mid;
+        } else {
+            left = mid;
+        }
+    }
+
+    return right;
+};
+
+/**
+ *
+ * @param {Number} m
+ * @param {Number} n
+ * @param {Number} k
+ * @param {Number} expect
+ * @return {Boolean}
+ */
+const check = (m, n, k, expect) => {
+    let count = 0;
+
+    for (let i = 1; i <= m; i++) {
+        count += Math.min(n, Math.floor(expect / i));
+
+        if (count >= k) {
+            return true;
+        }
+    }
+
+    return false;
+};
